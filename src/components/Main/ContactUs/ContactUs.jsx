dc96c4ed38624f1e62fca86aa6a68162f1382596
Lucas Ferreira <lukinhasgomes75@gmail.com>
2022-10-21 16:29:00 -0300

    formulario com sucesso para nosso email

--- src/components/Main/ContactUs/ContactUs.jsx
+++ src/components/Main/ContactUs/ContactUs.jsx
@@ -1,56 +1,105 @@
 import './ContactUs.scss'
 import emailjs from '@emailjs/browser';
+import { useRef, useState } from 'react';
 
 function ContactUs() {
-    const form = useRef();
+    // const form = useRef();
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        message: ""
+    })
+
+    const handleChangeFormData = (value, field) => {
+        setFormData({ ...formData, [field]: value })
+    }
 
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('service_7r3xhes', 'template_u6qscra', form.current, 'hWB5hdEdNicJWApdM')
+        emailjs.send(
+            'service_7r3xhes',
+            'template_u6qscra',
+            {
+                nome: formData.name,
+                email: formData.email,
+                telefone: formData.phone,
+                message: formData.message
+            },
+            'hWB5hdEdNicJWApdM'
+        )
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
                 console.log(error.text);
             });
-
-        return (
-            <section id='contato' className='contactUs'>
-                <div className="row input-container">
-                    <h1 className="title_contato">Fale Conosco</h1>
-
-                    <form method="post" action="#">
-                        <div className="style-form-input full">
-                            <input type="text" name="user_name" required />
-                            <label><i className="icon icon-user-1"></i>  Nome Completo</label>
-                        </div>
-
-                        <div className="style-form-input">
-                            <input type="text" name="user_email" required />
-                            <label><i className="icon icon-mail-1"></i> E-mail</label>
-                        </div>
-
-                        <div className="style-form-input right">
-                            <input type="text" name="assunto" required />
-                            <label><i className="icon icon-info-circled"></i> Telefone</label>
-                        </div>
-
-                        <div className="style-form-input full">
-                            <textarea name="message" required></textarea>
-                            <label><i className="icon icon-comments"></i> Mensagem</label>
-                        </div>
-
-                        <div className="style-form-input full">
-
-                            <button className="btn-submit" value={send}>Enviar</button>
-
-                        </div>
-                    </form>
-                </div>
-            </section>
-
-        )
     }
+    return (
+        <section id='contato' className='contactUs'>
+            <div className="row input-container">
+                <h1 className="title_contato">Fale Conosco</h1>
+
+                {/* <form method="post" action="#"> */}
+                <div className="style-form-input full">
+                    <input
+                        type="text"
+                        value={formData.name}
+                        onChange={
+                            (e) => handleChangeFormData(e.target.value, 'name')
+                        }
+                        required
+                    />
+                    <label><i className="icon icon-user-1"></i>  Nome Completo</label>
+                </div>
+
+                <div className="style-form-input">
+                    <input
+                        type="text"
+                        value={formData.email}
+                        onChange={
+                            (e) => handleChangeFormData(e.target.value, 'email')
+                        }
+                        required
+                    />
+                    <label><i className="icon icon-mail-1"></i> E-mail</label>
+                </div>
+
+                <div className="style-form-input right">
+                    <input
+                        type="text"
+                        value={formData.phone}
+                        onChange={
+                            (e) => handleChangeFormData(e.target.value, 'phone')
+                        }
+                        required
+                    />
+                    <label><i className="icon icon-info-circled"></i> Telefone</label>
+                </div>
+
+                <div className="style-form-input full">
+                    <textarea
+                        value={formData.message}
+                        onChange={
+                            (e) => handleChangeFormData(e.target.value, 'message')
+                        }
+                        required>
+
+                    </textarea>
+                    <label><i className="icon icon-comments"></i> Mensagem</label>
+                </div>
+
+                <div className="style-form-input full">
+
+                    <button className="btn-submit" type="button" onClick={sendEmail}>Enviar</button>
+
+                </div>
+                {/* </form> */}
+            </div>
+        </section>
+
+    )
 }
+
 
 export default ContactUs
