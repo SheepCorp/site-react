cc71645cf465d5410b715b6f76fc948bb711c289
Lucas Ferreira <lukinhasgomes75@gmail.com>
2022-10-21 11:10:59 -0300

    coloquei o formulario para funcionar pelo emailjs

--- src/components/Main/ContactUs/ContactUs.jsx
+++ src/components/Main/ContactUs/ContactUs.jsx
@@ -1,42 +1,56 @@
 import './ContactUs.scss'
+import emailjs from '@emailjs/browser';
 
 function ContactUs() {
-    return (
-        <section id='contato' className='contactUs'>
-            <div className="row input-container">
-                <h1 className="title_contato">Fale Conosco</h1>
+    const form = useRef();
 
-                <form method="post" action="#">
-                    <div className="style-form-input full">
-                        <input type="text" name="nome" required />
-                        <label><i className="icon icon-user-1"></i>  Nome Completo</label>
-                    </div>
+    const sendEmail = (e) => {
+        e.preventDefault();
 
-                    <div className="style-form-input">
-                        <input type="text" name="email" required />
-                        <label><i className="icon icon-mail-1"></i> E-mail</label>
-                    </div>
+        emailjs.sendForm('service_7r3xhes', 'template_u6qscra', form.current, 'hWB5hdEdNicJWApdM')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
 
-                    <div className="style-form-input right">
-                        <input type="text" name="assunto" required />
-                        <label><i className="icon icon-info-circled"></i> Telefone</label>
-                    </div>
+        return (
+            <section id='contato' className='contactUs'>
+                <div className="row input-container">
+                    <h1 className="title_contato">Fale Conosco</h1>
 
-                    <div className="style-form-input full">
-                        <textarea name="mensagem" required></textarea>
-                        <label><i className="icon icon-comments"></i> Mensagem</label>
-                    </div>
+                    <form method="post" action="#">
+                        <div className="style-form-input full">
+                            <input type="text" name="user_name" required />
+                            <label><i className="icon icon-user-1"></i>  Nome Completo</label>
+                        </div>
 
-                    <div className="style-form-input full">
+                        <div className="style-form-input">
+                            <input type="text" name="user_email" required />
+                            <label><i className="icon icon-mail-1"></i> E-mail</label>
+                        </div>
 
-                        <button className="btn-submit">Enviar</button>
+                        <div className="style-form-input right">
+                            <input type="text" name="assunto" required />
+                            <label><i className="icon icon-info-circled"></i> Telefone</label>
+                        </div>
 
-                    </div>
-                </form>
-            </div>
-        </section>
+                        <div className="style-form-input full">
+                            <textarea name="message" required></textarea>
+                            <label><i className="icon icon-comments"></i> Mensagem</label>
+                        </div>
 
-    )
+                        <div className="style-form-input full">
+
+                            <button className="btn-submit" value={send}>Enviar</button>
+
+                        </div>
+                    </form>
+                </div>
+            </section>
+
+        )
+    }
 }
 
 export default ContactUs
