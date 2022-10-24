f20c3520f9a5bc45f15421bd383d8f31584419ba
Lucas Ferreira <lukinhasgomes75@gmail.com>
2022-10-24 14:04:00 -0300

    pasta web removida e tudo funcionando

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from './components/Header/Header'
+import HomePage from './components/Main/HomePage/HomePage'
+import ForUs from './components/Main/ForUs/ForUs'
+import Projetos from './components/Main/Projetos/Projetos'
+import ContactUs from './components/Main/ContactUs/ContactUs'
+import Footer from './components/Footer/Footer'
+import Float from './components/Main/Float/Float'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HomePage />
+      <ForUs />
+      <Projetos />
+      <ContactUs />
+      <Footer />
+      <Float />
+    </>
+  )
+}
+
+export default App
--- src/components/Main/ForUs/Team/Team.jsx
+++ src/components/Main/ForUs/Team/Team.jsx
@@ -7,10 +7,9 @@ import pedro from '../../../images/cards/pedro.png'
 import lucas from '../../../images/cards/lucas.png'
 
 
-/*
+
 import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/scss/alice-carousel.scss";
-
 
 const settings = {
     items: [
@@ -30,51 +29,12 @@ const settings = {
     renderNextButton: true,
 }
 
-
-
 function Team() {
     return (
         <AliceCarousel {...settings} />
     )
 }
-*/
 
-import Slider from "react-slick";
-
-
-
-
-function Team() {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    };
-    return (
-        <Slider {...settings}>
-            <div>
-                <h3>1</h3>
-            </div>
-            <div>
-                <h3>2</h3>
-            </div>
-            <div>
-                <h3>3</h3>
-            </div>
-            <div>
-                <h3>4</h3>
-            </div>
-            <div>
-                <h3>5</h3>
-            </div>
-            <div>
-                <h3>6</h3>
-            </div>
-        </Slider>
-    );
-}
 
 
 
